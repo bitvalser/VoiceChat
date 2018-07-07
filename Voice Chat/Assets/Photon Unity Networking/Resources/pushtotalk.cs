dc2d554b0765f79358fedb4eef7e17a0d9abacd0
StreinGon <artyom199788@yandex.by>
2018-07-07 11:30:09 +0300

    рабочий войс

---
+++ Voice Chat/Assets/Photon Unity Networking/Resources/pushtotalk.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace ExitGames.Demos.DemoPunVoice
+{
+	using Client.Photon.LoadBalancing;
+
+
+	public class pushtotalk : Photon.MonoBehaviour {
+		private PhotonVoiceRecorder rec;
+		// Use this for initialization
+		void Start () {
+			rec = GetComponent<PhotonVoiceRecorder> ();
+			rec.Transmit = false;
+		}
+		
+		// Update is called once per frame
+		public void Update () {
+			if (Input.GetKeyDown(KeyCode.Space)) {
+				rec.Transmit = true;
+			}
+			else if (Input.GetKeyUp(KeyCode.Space))
+			{
+				rec.Transmit = false;
+			}
+		}
+	}
+}
