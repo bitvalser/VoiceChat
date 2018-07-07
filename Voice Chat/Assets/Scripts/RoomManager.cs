2f1f0ca180a543f35722f08c81a2445a5938e264
VALERA\Valera <bitvalser@gmail.com>
2018-07-07 18:31:54 +0300

    button fix

--- Voice Chat/Assets/Scripts/RoomManager.cs
+++ Voice Chat/Assets/Scripts/RoomManager.cs
@@ -1,18 +1,20 @@
 ﻿using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RoomManager : Photon.MonoBehaviour {
 
     private int players;
     public Text output;
     private string text;
+    public Button back;
 
     // Use this for initialization
     void Start () {
         PhotonNetwork.ConnectUsingSettings("1.0");
         output.text = PhotonNetwork.playerName;
         players = PhotonNetwork.countOfPlayersInRooms;
-        
+        back.onClick.AddListener(BackToLobby);
     }
 
     private void Update()
@@ -28,6 +30,11 @@ public class RoomManager : Photon.MonoBehaviour {
     private void OnGUI()
     {
         GUILayout.Label("\n" + PhotonNetwork.connectionStateDetailed.ToString() + text);
+    }
+
+    public void BackToLobby()
+    {
+        SceneManager.LoadScene("Hub");
     }
 
     void OnJoinedLobby()
