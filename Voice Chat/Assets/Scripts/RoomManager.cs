646434a87b57feefe40dd548c344d8984b22e283
VALERA\Valera <bitvalser@gmail.com>
2018-07-07 15:11:32 +0300

    test fix voice

--- Voice Chat/Assets/Scripts/RoomManager.cs
+++ Voice Chat/Assets/Scripts/RoomManager.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 
 public class RoomManager : Photon.MonoBehaviour {
 
+    private int players;
+
 	// Use this for initialization
 	void Start () {
+        PhotonNetwork.ConnectUsingSettings("1.0");
         Debug.Log("Connected");
+        players = 1;
         PhotonNetwork.Instantiate("testPrefab", Vector3.zero, Quaternion.identity, 0);
     }
-	
+
+    private void Update()
+    {
+        if(PhotonNetwork.countOfPlayersInRooms > players)
+        {
+            players++;
+            Debug.Log("p++");
+            PhotonNetwork.Instantiate("testPrefab", Vector3.zero, Quaternion.identity, 0);
+        }
+    }
+
     void OnJoinedLobby()
     {
         //PhotonNetwork.CreateRoom("va", new RoomOptions(), TypedLobby.Default);
