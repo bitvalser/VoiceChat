48ff815edae256b2a1502ba95ac1db0ba79d583e
VALERA\Valera <bitvalser@gmail.com>
2018-07-08 15:10:51 +0300

    sound add

--- Voice Chat/Assets/Photon Unity Networking/Resources/pushtotalk.cs
+++ Voice Chat/Assets/Photon Unity Networking/Resources/pushtotalk.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 namespace ExitGames.Demos.DemoPunVoice
 {
 	using Client.Photon.LoadBalancing;
@@ -11,51 +12,26 @@ namespace ExitGames.Demos.DemoPunVoice
 	public class pushtotalk : Photon.MonoBehaviour {
 
 		private PhotonVoiceRecorder rec;
-        public Sprite NoVoice;
-        public Sprite Voice;
-        public Button btnVoice;
-        private bool isPress;
+       
 
         private void Awake()
         {
             DontDestroyOnLoad(gameObject.transform);
         }
-        // Use this for initialization
+        
         void Start () {
 			rec = GetComponent<PhotonVoiceRecorder> ();
 			rec.Transmit = false;
-            isPress = false;
         }
 
         public void Update () {
-            
-			if (Input.GetKeyDown(KeyCode.Space) || isPress) {
-				rec.Transmit = true;
+			if (Input.GetKeyDown(KeyCode.Space)) {
+                rec.Transmit = true;
             }
 			else if (Input.GetKeyUp(KeyCode.Space))
 			{
-				rec.Transmit = false;
+                rec.Transmit = false;
             }
-
-            if (Input.GetKey(KeyCode.Space))
-            {
-                btnVoice.image.sprite = Voice;
-            }
-            else
-            {
-                btnVoice.image.sprite = NoVoice;
-            }
-        }
-
-        public void OnPointerDown()
-        {
-            isPress = true;
-            btnVoice.image.sprite = Voice;
-        }
-        public void onPointerUp()
-        {
-            isPress = false;
-            btnVoice.image.sprite = NoVoice;
         }
     }
 }
---
+++ Voice Chat/Assets/Scripts/TouchRace.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+public class TouchRace : MonoBehaviour
+{
+
+    public bool isRacePressed = false;
+    public bool isbrakePressed = false;
+    public Button btnVoice;
+    public GameObject startVoice;
+    public GameObject endVoice;
+    public Sprite NoVoice;
+    public Sprite Voice;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (isRacePressed || Input.GetKey(KeyCode.Space))
+        {
+            btnVoice.image.sprite = Voice;
+            if(Input.GetKeyDown(KeyCode.Space)) Destroy(Instantiate(startVoice, Vector3.zero, Quaternion.identity), 0.5f);
+        }
+
+        else if (!isRacePressed)
+        {
+            btnVoice.image.sprite = NoVoice;
+            if (Input.GetKeyUp(KeyCode.Space)) Destroy(Instantiate(endVoice, Vector3.zero, Quaternion.identity), 0.5f);
+        }
+    }
+    public void onPointerDownRaceButton()
+    {
+        isRacePressed = true;
+        btnVoice.image.sprite = Voice;
+        Destroy(Instantiate(startVoice, Vector3.zero, Quaternion.identity), 0.5f);
+    }
+    public void onPointerUpRaceButton()
+    {
+        isRacePressed = false;
+        btnVoice.image.sprite = NoVoice;
+        Destroy(Instantiate(endVoice, Vector3.zero, Quaternion.identity), 0.5f);
+    }
+}
